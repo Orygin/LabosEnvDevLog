2b20e46840d68f89330a07ed91f644212ed6dc09
Louis Geuten <louisgeuten@gmail.com>
2016-10-10 10:24:13 +0200

    Labo 5

---
+++ Labo5/Model/WeatherService.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labo5.Model
+{
+    class WeatherService
+    {
+        public async Task<IEnumerable<WeatherForecast>> GetForecast()
+        {
+            var wc = new HttpClient();
+            var weather = await wc.GetStringAsync("http://api.openweathermap.org/data/2.5/forecast?id=2790472&mode=json&APPID=e6f6a715dcd801d9ea1678adb73ef17d&lang=fr");
+            var rawWeather = JObject.Parse(weather);
+            var forecast = rawWeather["list"].Children().Select(d => new WeatherForecast()
+            {
+                Date = d["dt_txt"].Value<DateTime>(),
+                MinTemp = d["main"]["temp_min"].Value<double>(),
+                MaxTemp = d["main"]["temp_max"].Value<double>(),
+                WeatherDescription = d["weather"].First["description"].Value<string>(),
+                WindSpeed = d["wind"]["speed"].Value<double>()
+            });
+            return forecast;
+        }
+    }
+}
---
+++ Labo5/ViewModel/MainViewModel.cs
@@ -0,0 +1,65 @@
+﻿using GalaSoft.MvvmLight;
+using Labo5.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labo5.ViewModel
+{
+    public class MainViewModel : ViewModelBase, INotifyPropertyChanged
+    {
+        private ObservableCollection<WeatherForecast> _forecast = null;
+
+        public ObservableCollection<WeatherForecast> Forecast
+        {
+            get
+            {
+                return _forecast;
+            }
+            set
+            {
+                if(_forecast == value)
+                {
+                    return;
+                }
+                _forecast = value;
+                RaisePropertyChanged("Forecast");
+            }
+        }
+
+        public MainViewModel()
+        {
+            if (IsInDesignMode)
+            {
+                var forecast = new Forecast() { Cityname = "Namur" };
+                var weatherForecasts = new List<WeatherForecast>();
+                for(int i= 0; i < 40; i++)
+                {
+                    weatherForecasts.Add(new WeatherForecast()
+                    {
+                        Date = DateTime.Now.AddDays(i),
+                        MaxTemp = 5 + i / 2,
+                        MinTemp = i / 2,
+                        WeatherDescription = "Peu de nuages",
+                        WindSpeed = i % 7
+                    });
+                }
+                forecast.WeatherForecasts = weatherForecasts;
+                Forecast = new ObservableCollection<WeatherForecast>(weatherForecasts);
+            }
+            else
+                InitializeAsync();
+        }
+
+        private async Task InitializeAsync()
+        {
+            var service = new WeatherService();
+            var forecast = await service.GetForecast();
+            Forecast = new ObservableCollection<WeatherForecast>(forecast);
+        }
+    }
+}
